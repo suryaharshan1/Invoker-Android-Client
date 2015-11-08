55a526f5cc63809b17e0cd54c3aab438e7dae7e7
suryaharshan1 <suryaharshan1@gmail.com>
2015-11-09 00:05:49 +0530

    Update RegisterActivity with UI and SharedPreferences

--- app/src/main/java/com/windroilla/invoker/RegisterActivity.java
+++ app/src/main/java/com/windroilla/invoker/RegisterActivity.java
@@ -1,17 +1,21 @@
 package com.windroilla.invoker;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
     public final static String APP_PREF_KEY = "INVOKER_PREF_KEY";
     public final static String FLAG_FIRST_USE = "INVOKER_FIRST_USE";
+    public final static String TAG = "INVOKER_REGISTER";
+    EditText phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +23,20 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        phone = (EditText) findViewById(R.id.register_phone_editText);
+        phone.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View view) {
-                SharedPreferences sp = getSharedPreferences(APP_PREF_KEY, MODE_PRIVATE);
-                SharedPreferences.Editor editor = sp.edit();
-                editor.putBoolean(FLAG_FIRST_USE, false);
-                editor.commit();
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    SharedPreferences sp = getSharedPreferences(APP_PREF_KEY, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putBoolean(FLAG_FIRST_USE, false);
+                    editor.commit();
+                    //TODO make network call to store the details in server
+                    startActivity(new Intent(getBaseContext(), MainActivity.class));
+                    finish();
+                }
+                return false;
             }
         });
     }
