6affea60552ee5abd9bf2c8d91b6765d770f4902
suryaharshan1 <suryaharshan1@gmail.com>
2015-11-15 19:06:34 +0530

    TouchBlockService view options update

--- app/src/main/java/com/windroilla/invoker/TouchBlockService.java
+++ app/src/main/java/com/windroilla/invoker/TouchBlockService.java
@@ -106,7 +106,12 @@ public class TouchBlockService extends Service implements View.OnTouchListener {
 
         public void hideStatus() {
             // Hide the status bar.
-            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
             mBlockerView.setSystemUiVisibility(uiOptions);
         }
 
