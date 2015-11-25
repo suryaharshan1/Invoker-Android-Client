6e757f56ef871f7f9175f5219c1ee810b2059f17
suryaharshan1 <suryaharshan1@gmail.com>
2015-11-25 23:23:45 +0530

    Setting up OnBootService with AlarmManager

--- app/src/main/java/com/windroilla/invoker/OnBootService.java
+++ app/src/main/java/com/windroilla/invoker/OnBootService.java
@@ -1,8 +1,11 @@
 package com.windroilla.invoker;
 
+import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.IBinder;
@@ -10,10 +13,13 @@ import android.util.Log;
 
 import com.windroilla.invoker.data.BlocktimeContract;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class OnBootService extends Service {
 
+    public final static String APP_PREF_KEY = "INVOKER_PREF_KEY";
+    public final static String INVOKER_SERVICE_COUNT = "INVOKER_SERVICE_COUNT";
     private static final String TAG = "INVOKER_ONBOOTSERVICE";
     private static final String[] BLOCKTIME_COLUMNS = {
             BlocktimeContract.BlocktimeEntry.TABLE_NAME + "." + BlocktimeContract.BlocktimeEntry._ID,
@@ -21,8 +27,11 @@ public class OnBootService extends Service {
             BlocktimeContract.BlocktimeEntry.COLUMN_END_TIME,
             BlocktimeContract.BlocktimeEntry.COLUMN_CREATED_TIME
     };
+    private static final int COL_BLOCKTIME_ID = 0;
+    private static final int COL_BLOCKTIME_STARTTIME = 1;
+    private static final int COL_BLOCKTIME_ENDTIME = 2;
+    private static final int COL_BLOCKTIME_UPDATETIME = 3;
     Cursor mCursor;
-
 
     public OnBootService() {
     }
@@ -45,7 +54,8 @@ public class OnBootService extends Service {
         if (null == mCursor) {
             Log.e(TAG, "some error while querying the content provider");
             // If the Cursor is empty, the provider found no matches
-        } else if (mCursor.getCount() < 1) {
+        } else {
+            if (mCursor.getCount() < 1) {
 
             /*
              * Insert code here to notify the user that the search was unsuccessful. This isn't necessarily
@@ -53,16 +63,56 @@ public class OnBootService extends Service {
              * search term.
              */
 
-        } else {
-            // Insert code here to do something with the results
-            mCursor.moveToFirst();
-            while (!mCursor.isAfterLast()) {
-                //Set alarms for the intervals which are greater than or in between the current interval
-                mCursor.moveToNext();
+            } else {
+                // Insert code here to do something with the results
+                mCursor.moveToFirst();
+                while (!mCursor.isAfterLast()) {
+                    //Set alarms for the intervals which are greater than or in between the current interval
+                    try {
+                        if (System.currentTimeMillis() < formatter.parse(mCursor.getString(COL_BLOCKTIME_STARTTIME)).getTime()) {
+                            Intent startIntent = new Intent(getBaseContext(), AlarmReceiver.class);
+                            startIntent.setAction("com.windroilla.invoker.blockservice.start");
+                            Intent endIntent = new Intent(getBaseContext(), AlarmReceiver.class);
+                            endIntent.setAction("com.windroilla.invoker.blockservice.stop");
+                            PendingIntent pendingStartIntent = PendingIntent.getBroadcast(getApplicationContext(), mCursor.getInt(COL_BLOCKTIME_ID), startIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                            PendingIntent pendingEndIntent = PendingIntent.getBroadcast(getApplicationContext(), -mCursor.getInt(COL_BLOCKTIME_ID), endIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                            mgrAlarm.set(AlarmManager.RTC_WAKEUP,
+                                    formatter.parse(mCursor.getString(COL_BLOCKTIME_STARTTIME)).getTime(),
+                                    pendingStartIntent);
+                            Log.d(TAG, formatter.parse(mCursor.getString(COL_BLOCKTIME_STARTTIME)).getTime() + " " + System.currentTimeMillis() + " " + (formatter.parse(mCursor.getString(COL_BLOCKTIME_STARTTIME)).getTime() - System.currentTimeMillis()));
+                            Log.d(TAG, formatter.parse(mCursor.getString(COL_BLOCKTIME_ENDTIME)).getTime() + " " + System.currentTimeMillis() + " " + (formatter.parse(mCursor.getString(COL_BLOCKTIME_ENDTIME)).getTime() - System.currentTimeMillis()));
+                            mgrAlarm.set(AlarmManager.RTC_WAKEUP,
+                                    formatter.parse(mCursor.getString(COL_BLOCKTIME_ENDTIME)).getTime(),
+                                    pendingEndIntent);
+                        } else if (System.currentTimeMillis() >= formatter.parse(mCursor.getString(COL_BLOCKTIME_STARTTIME)).getTime() && System.currentTimeMillis() < formatter.parse(mCursor.getString(COL_BLOCKTIME_ENDTIME)).getTime()) {
+                            Intent endIntent = new Intent(getBaseContext(), AlarmReceiver.class);
+                            endIntent.setAction("com.windroilla.invoker.blockservice.stop");
+                            PendingIntent pendingEndIntent = PendingIntent.getBroadcast(getApplicationContext(), -mCursor.getInt(COL_BLOCKTIME_ID), endIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                            Log.d(TAG, formatter.parse(mCursor.getString(COL_BLOCKTIME_ENDTIME)).getTime() + " " + System.currentTimeMillis() + " " + (formatter.parse(mCursor.getString(COL_BLOCKTIME_ENDTIME)).getTime() - System.currentTimeMillis()));
+                            mgrAlarm.set(AlarmManager.RTC_WAKEUP,
+                                    formatter.parse(mCursor.getString(COL_BLOCKTIME_ENDTIME)).getTime(),
+                                    pendingEndIntent);
+                            SharedPreferences sp = getApplicationContext().getSharedPreferences(APP_PREF_KEY, Activity.MODE_PRIVATE);
+                            int serviceCount = sp.getInt(INVOKER_SERVICE_COUNT, 0);
+                            SharedPreferences.Editor editor = sp.edit();
+                            Log.d(TAG, "OnBootService OnCreate with serviceCount " + serviceCount);
+                            if (serviceCount == 0) {
+                                Intent i = new Intent(getApplicationContext(), TouchBlockService.class);
+                                getApplicationContext().startService(i);
+                                Log.d(TAG, "OnBootService OnCreate start TouchBlockService");
+                            }
+                            serviceCount++;
+                            editor.putInt(INVOKER_SERVICE_COUNT, serviceCount);
+                            editor.commit();
+                        }
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    mCursor.moveToNext();
+                }
             }
-
         }
-
+        stopSelf();
     }
 
     @Override
